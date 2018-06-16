ea2b18a675c36f019660032d480bbaa62431139e
Marcin Wesel <marcin.wesel@pl.abb.com>
2018-06-16 11:21:16 +0200

    Code after first lab.

---
+++ Testing/Testing/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Testing
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+        }
+
+        internal async void btnCheckUrl_Clicked(object sender, EventArgs e)
+        {
+           var url = entryUrl.Text;
+           await Navigation.PushAsync(new HttpClientPage(url));
+        }
+
+        internal async void btnOpenUrl_Clicked(object sender, EventArgs e)
+        {
+            var url = entryUrl.Text;
+            if (await DisplayAlert("Czy na pewno?", "Czy na pewno chcesz otworzyć stronę? " + url, "TAK", "NIE"))
+            {
+                await Navigation.PushAsync(new WebViewPage(url));
+            }
+            else
+            {
+                await DisplayAlert("Dobrze", "Nie otwieram strony", "OK");
+            }
+        }
+    }
+}
